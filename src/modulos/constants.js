bde34bf923f851fd6d6d33e3b9b1715f2e8b938b
Victor Moura <victormoura310@gmail.com>
2025-05-17 21:16:26 -0300

    adicionado mais duas constants, getTaxRate e getAssetRentability

--- src/modulos/constants.js
+++ src/modulos/constants.js
@@ -6,3 +6,22 @@ export  const taxs = {
     LCA: 14,
     CDB: 13.15,
   };
+
+  export const getTaxRate = (months) => {
+  const taxRates = [
+    { limit: 6, rate: 0.225 },
+    { limit: 12, rate: 0.2 },
+    { limit: 24, rate: 0.175 },
+    { limit: Infinity, rate: 0.15 },
+  ];
+  return taxRates.find(({ limit }) => months <= limit).rate;
+};
+
+export const getAssetRentability = (asset) =>
+  ({
+    "Tesouro Prefixado": taxs.TESOURO_PREFIXADO,
+    "Tesouro IPCA+": taxs.TESOURO_IPCA,
+    "Tesouro Selic": taxs.TESOURO_SELIC,
+    "LCI e LCA": taxs.LCI,
+    "CDB e LC": taxs.CDB,
+  }[asset] || 0);
