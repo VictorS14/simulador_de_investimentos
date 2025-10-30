0a8dd17b055d767f82a4c6dc2d334e41ee0a29f3
Victor Moura <victormoura310@gmail.com>
2025-10-30 20:45:04 -0300

    feat: Implement SimulationForm

--- my-simulator/src/App.jsx
+++ my-simulator/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from "react"
 import bankLogo from "./assets/bank.svg"
 import './App.css'
 import { AssetOptions } from "./components/AssetOptions";
+import { SimulationForm } from "./components/SimulationForm";
 
 
 function App() {
   const [selectedAsset, setSelectedAsset] = useState('Tesouro Prefixado');
   const [showResults, setShowResults] = useState(false);
+  const [simulationData, setSimulationData] = useState(null);
 
   const handleAssetChange = (asset) => {
     setSelectedAsset(asset)
+  }
+
+  const handleSimulationComplete = (data) => {
+    setSimulationData(data);
+    setShowResults(true);
   }
 
 
@@ -22,12 +29,17 @@ function App() {
         alt="bank logo"/> 
       </header>
       <h2 className="text-blue-950 text-[min(5vw,36px)]">Simulador de Investimentos</h2>
-      <div className="bg-gray-100 flex flex-col items-center px-5 py-6 rounded-sm gap-5">
+      <div className="bg-gray-200 flex flex-col items-center px-5 py-6 rounded-sm gap-5">
         {!showResults && (
           <>
             <AssetOptions 
             selectedAsset={selectedAsset}
             onAssetChange={handleAssetChange}
+            />
+
+            <SimulationForm 
+              selectedAsset={selectedAsset}
+              onSimulationComplete={handleSimulationComplete}
             />
           </>
         )}
