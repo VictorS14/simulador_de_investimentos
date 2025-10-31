b9b16a7e4c5642f2d898003a75e87f51991b9152
Victor Moura <victormoura310@gmail.com>
2025-10-31 13:00:27 -0300

    style: fix some width in layout

--- my-simulator/src/App.jsx
+++ my-simulator/src/App.jsx
@@ -24,41 +24,42 @@ function App() {
   const handleResetSimulation = () => {
     setShowResults(false);
     setSimulationData(null);
+    setSelectedAsset("Tesouro Prefixado")
   }
 
 
   return (
-    <div className="w-full max-w-3xl flex flex-col gap-10">
-      <header className="w-12 h-12 flex items-center justify-center"> 
-        <img 
-        className="w-full h-full"
-        src={bankLogo} 
-        alt="bank logo"/> 
-      </header>
-      <h2 className="text-blue-950 text-[min(5vw,36px)]">Simulador de Investimentos</h2>
-      <div className="min-w-80 bg-gray-200 flex flex-col items-center px-5 py-6 rounded-sm gap-5">
-        {!showResults && (
-          <>
-            <AssetOptions 
-            selectedAsset={selectedAsset}
-            onAssetChange={handleAssetChange}
-            />
-
-            <SimulationForm 
+      <div className="min-w-full max-w-3xl flex flex-col gap-10">
+        <header className="w-12 h-12 flex items-center justify-center"> 
+          <img 
+          className="w-full h-full"
+          src={bankLogo} 
+          alt="bank logo"/> 
+        </header>
+        <h2 className="text-blue-950 font-semibold text-[min(5vw,36px)]">Simulador de Investimentos</h2>
+        <div className="bg-gray-200 flex flex-col items-center px-5 py-6 rounded-sm gap-5">
+          {!showResults && (
+            <>
+              <AssetOptions 
               selectedAsset={selectedAsset}
-              onSimulationComplete={handleSimulationComplete}
-            />
-          </>
-        )}
+              onAssetChange={handleAssetChange}
+              />
 
-        {showResults && (
-          <SimulationResults 
-          data={simulationData}
-          onReset={handleResetSimulation}
-          />
-        )}
+              <SimulationForm 
+                selectedAsset={selectedAsset}
+                onSimulationComplete={handleSimulationComplete}
+              />
+            </>
+          )}
+
+          {showResults && (
+            <SimulationResults 
+            data={simulationData}
+            onReset={handleResetSimulation}
+            />
+          )}
+        </div>
       </div>
-    </div>
   )
 }
 
